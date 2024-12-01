cfef59f592a01a40386a042b194a9f46685594e5
Seth Stadick <sstadick@gmail.com>
2024-12-01 07:56:21 -0700

    feat(day1): parts 1 and 2

--- day-01/benches/benchmarks.rs
+++ day-01/benches/benchmarks.rs
@@ -7,16 +7,10 @@ fn main() {
 
 #[divan::bench]
 fn part1() {
-    part1::process(divan::black_box(
-        "../input1.txt",
-    ))
-    .unwrap();
+    part1::process(divan::black_box("./input1.txt")).unwrap();
 }
 
 #[divan::bench]
 fn part2() {
-    part2::process(divan::black_box(
-        "../input2.txt",
-    ))
-    .unwrap();
+    part2::process(divan::black_box("./input2.txt")).unwrap();
 }
