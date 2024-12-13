b82f8aefc1d2394bfe711066effec1ee85e7440a
Seth <sstadick@gmail.com>
2024-12-13 10:54:18 -0700

    feat(day1): parts 1 and 2 (#1)

--- day-01/src/part1.rs
+++ day-01/src/part1.rs
@@ -1,9 +1,44 @@
 use std::fmt::Debug;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
 use std::path::Path;
 
+use anyhow::Context;
+
+pub fn naive_parse_file<P: AsRef<Path>>(file: P) -> anyhow::Result<(Vec<usize>, Vec<usize>)> {
+    let reader = BufReader::new(File::open(file)?);
+    let mut firsts = vec![];
+    let mut seconds = vec![];
+    for line in reader.lines() {
+        let line = line?;
+        let first = line
+            .split(" ")
+            .next()
+            .context("Failed to parse first number")?
+            .parse::<usize>()?;
+        let second = line
+            .rsplit(" ")
+            .next()
+            .context("Failed to parse second number")?
+            .parse::<usize>()?;
+        firsts.push(first);
+        seconds.push(second);
+    }
+    firsts.sort_unstable();
+    seconds.sort_unstable();
+    Ok((firsts, seconds))
+}
+
 #[tracing::instrument]
-pub fn process<P: AsRef<Path> + Debug>(_input: P) -> anyhow::Result<String> {
-    todo!("day 01 - part 1");
+pub fn process<P: AsRef<Path> + Debug>(input: P) -> anyhow::Result<String> {
+    let (first_side, second_side) = naive_parse_file(input)?;
+    let ret = first_side
+        .into_iter()
+        .zip(second_side.into_iter())
+        .map(|(a, b)| a.abs_diff(b))
+        .sum::<usize>();
+
+    Ok(ret.to_string())
 }
 
 #[cfg(test)]
@@ -20,13 +55,17 @@ mod tests {
 
     #[test]
     fn test_process() -> anyhow::Result<()> {
-        todo!("haven't built test yet");
-        let input = "";
+        let input = "3   4
+4   3
+2   5
+1   3
+3   9
+3   3";
         let dir = TempDir::new("test")?;
         let file = dir.path().join("input1.txt");
         write_to_file(input, &file);
 
-        assert_eq!("", process(&file)?);
+        assert_eq!("11", process(&file)?);
         Ok(())
     }
 }
--- day-01/src/part2.rs
+++ day-01/src/part2.rs
@@ -1,9 +1,44 @@
+use std::collections::HashMap;
 use std::fmt::Debug;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
 use std::path::Path;
 
+use anyhow::Context;
+
+pub fn naive_parse_file<P: AsRef<Path>>(
+    file: P,
+) -> anyhow::Result<(Vec<usize>, HashMap<usize, usize>)> {
+    let reader = BufReader::new(File::open(file)?);
+    let mut left = vec![];
+    let mut map = HashMap::default();
+    for line in reader.lines() {
+        let line = line?;
+        let first = line
+            .split(" ")
+            .next()
+            .context("Failed to parse first number")?
+            .parse::<usize>()?;
+        let second = line
+            .rsplit(" ")
+            .next()
+            .context("Failed to parse second number")?
+            .parse::<usize>()?;
+        *map.entry(second).or_insert(0) += 1;
+        left.push(first);
+    }
+    Ok((left, map))
+}
+
 #[tracing::instrument]
-pub fn process<P: AsRef<Path> + Debug>(_input: P) -> anyhow::Result<String> {
-    todo!("day 01 - part 2");
+pub fn process<P: AsRef<Path> + Debug>(input: P) -> anyhow::Result<String> {
+    let (left_side, right_side_counts) = naive_parse_file(input)?;
+    let ret = left_side
+        .into_iter()
+        .map(|left| right_side_counts.get(&left).unwrap_or(&0) * left)
+        .sum::<usize>()
+        .to_string();
+    Ok(ret)
 }
 
 #[cfg(test)]
@@ -20,13 +55,17 @@ mod tests {
 
     #[test]
     fn test_process() -> anyhow::Result<()> {
-        todo!("haven't built test yet");
-        let input = "";
+        let input = "3   4
+4   3
+2   5
+1   3
+3   9
+3   3";
         let dir = TempDir::new("test")?;
         let file = dir.path().join("input2.txt");
         write_to_file(input, &file);
 
-        assert_eq!("", process(&file)?);
+        assert_eq!("31", process(&file)?);
         Ok(())
     }
 }
